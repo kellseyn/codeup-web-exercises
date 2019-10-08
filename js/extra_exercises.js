1cd357f362b762b02c065a01a583c3e6169bc6ae
Kellsey Neagley <kellseyneagley@gmail.com>
2019-10-08 14:57:39 -0500

    added footer nav to resume

---
+++ js/extra_exercises.js
@@ -0,0 +1,144 @@
+"use strict";
+
+function isOdd(number) {
+
+}
+function isEven(number) {
+
+}
+function identity(input) {
+
+}
+function isFive(input) {
+
+}
+function addFive(input) {
+
+}
+function isMultipleOfFive(input) {
+
+}
+function isThree(input) {
+
+}
+function isMultipleOfThreeAndFive(input) {
+
+}
+function isMultipleOf(target, n) {
+
+}
+function isTrue(boolean) {
+
+}
+function isFalse(boolean) {
+
+}
+function isTruthy(input) {
+
+}
+function isFalsy(input) {
+
+}
+function isVowel(letter) {
+
+}
+function isConsonant(letter) {
+
+}
+isCapital(letter)
+function isLowerCase(letter) {
+
+}
+function hasLowerCase(string) {
+
+}
+function isSpace(letter) {
+
+}
+function isZero(number){
+
+}
+function notZero(input){
+
+}
+function  lowerCase(string){
+
+}
+function  double(n){
+
+}
+function  triple(n){
+
+}
+function  quadruple(n){
+
+}
+function  half(n){
+
+}
+function  subtract(a, b){
+
+}
+function  multiply(a, b) {
+
+}
+function  divide(a, b){
+
+}
+function  remainder(a, b){
+
+}
+function modulo(a, b){
+
+}
+function  cube(n){
+
+}
+function  squareRoot(n){
+
+}
+function  cubeRoot(n){
+
+}
+function  invertSign(number){
+
+}
+function  degreesToRadians(number){
+
+}
+function  radiansToDegrees(number){
+
+}
+function isBlank(input) {
+
+}
+function trim(string){
+
+}
+function areEqual(input1, input2){
+
+}
+function areIdentical(input1, input2){
+
+}
+function  not(input){
+
+}
+function notNot(input){
+
+}
+function  and(predicate1, predicate2){
+
+}
+function or(predicate1, predicate2){
+
+}
+function reverseString(string){
+
+}
+function absoluteValue(number){
+
+}
+function rollDice(sides){
+
+}
--- js/portfolio-resume.js
+++ js/portfolio-resume.js
@@ -1,19 +1,23 @@
 "use strict";
 
-$(function () {
-    var $lis = $('.header-featured-blocks li'),
-        $cur = $lis.first().addClass('active'),
-        $next = $cur.next().addClass('next');
-    $cur.fadeIn(500);
-    setInterval(function () {
-        $cur.fadeOut({duration: 500,queue: false}).animate({marginLeft:10}).removeClass('active');
-        $cur = $next.removeClass('next').css({marginLeft:-10}).fadeIn({duration: 500,queue: false}).animate({marginLeft:0}).addClass('active');
-        $next = $cur.next();
-        if (!$next.length) {
-            $next = $lis.first();
-        }
-        $next.addClass('next');
-    }, 5000);
+$(document).ready(function(){
+
+
+    $(function () {
+        var $lis = $('.header-featured-blocks li'),
+            $cur = $lis.first().addClass('active'),
+            $next = $cur.next().addClass('next');
+        $cur.fadeIn(500);
+        setInterval(function () {
+            $cur.fadeOut({duration: 500,queue: false}).animate({marginLeft:10}).removeClass('active');
+            $cur = $next.removeClass('next').css({marginLeft:-10}).fadeIn({duration: 500,queue: false}).animate({marginLeft:0}).addClass('active');
+            $next = $cur.next();
+            if (!$next.length) {
+                $next = $lis.first();
+            }
+            $next.addClass('next');
+        }, 5000);
+    });
 });
 
 // $.fn.infiniteScrollUp=function(){
