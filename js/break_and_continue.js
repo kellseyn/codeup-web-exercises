b469fddefd421296003bbd8aaba042ec5626ff1e
Kellsey Neagley <kellseyneagley@gmail.com>
2019-10-16 14:05:03 -0500

    walked thru loops exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,18 @@
 "use strict";
 
+while(true){
+    var num = parseInt(prompt("Pick an odd number between 1 and 50"));
+    if (num % 2 === 0 && <= 50 && num >= 1){
+        break;
+    }
+}
+
 for (i = 1; i < 50; i += 2){
+
     var odd = (i % 2 !== 0);
     console.log("Here is an odd number: " + i);
-    if (i === 25){
-        console.log("Yikes! Skipping number: " + (i+=2));
+    if (i === num){
+        console.log("Yikes! Skipping number: ");
         continue;
     }
 }
--- js/while.js
+++ js/while.js
@@ -130,17 +130,20 @@ while(i < 17){
 }
 
 // This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
+var allCones = Math.floor(Math.random() * 50) + 50;
 // // This expression will generate a random number between 1 and 5
-// Math.floor(Math.random() * 5) + 1;
 //
 //
+//
 do {
-    console.log("I can't sell you 90, I don't have enough");
-} while (allCones < 90);
-do {
-    console.log("I can sell you 70 cones");
-} while (allCones < 70);
-do {
-    console.log("I can go home now!");
-} while (allCones = 100);
+    var conesBought = Math.floor(Math.random() * 5) + 1;
+    if (allCones > conesBought) {
+        allCones -= conesBought;
+        console.log("customer bought " + conesBought + " cones. There are " + allCones + " cones left in inventory.");
+    } else if (allCones < conesBought) {
+        console.log("Sorry I can't sell you " + conesBought + " cones. I only have " + allCones + " cones left in inventory.");
+    } else{
+        allCones -= conesBought;
+        console.log("Yay I can go home. I'm all sold out!");
+    }
+}while (allCones > 0);
