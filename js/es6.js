0e201acea9643eab815a3b7cf19466592eaf7abb
Kellsey Neagley <kellseyneagley@gmail.com>
2019-11-12 12:05:14 -0600

    completed es6 lecture and exercise

--- js/es6.js
+++ js/es6.js
@@ -33,50 +33,74 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'kellsey';
+const email = 'kellseyneagley@gmail.com';
+const languages = ['html', 'css', 'javascript'];
+
+const myInfo = {
+  name,
+  email,
+  languages
+};
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+  name,
+  email,
+  languages
 });
+const contact = ({name, email}) =>{
+    console.log(name, email);
+};
+contact(myInfo);
+
+
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-  return emails.push(user.email);
-});
-users.forEach(function(user) {
-  return names.push(user.name);
-});
+// users.forEach(function(user) {
+//   return emails.push(user.email);
+// });
+// users.forEach(function(user) {
+//   return names.push(user.name);
+// });
+
+const getEmail = (n) => {for (let user of users) {
+    return n.push(user.email);
+}};
+const getName = (n) => {for (let user of users){
+    return n.push(user.name);
+
+}};
+getName(users);
+getEmail(users);
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+  const {name, email, languages} = user;
+  // const email = user.email;
+  // const languages = user.languages;
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  developers.push(`${name}'s email is ${email} ${name} knows ${languages}`);
 });
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+for (let developer of developers){
 
   // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-});
-list += '</ul>';
+  `${list} += '<li>' ${developer} '</li>'`;
+}
+`${list} += '</ul>'`;
+
+console.log(developers);
--- js/intro-to-es6.js
+++ js/intro-to-es6.js
@@ -8,18 +8,18 @@
 //     Using the ES6 exponentiation Operator, refactor the following:
 
 
-// var x = Math.pow(4,5);
-// console.log(x);
-//
-// let random = Math.floor(Math.random() * (Math.pow(2,3)));
-// console.log(random);
+let x = 4 ** 5;
+console.log(x);
+
+let random = Math.floor(Math.random() * (2 ** 3));
+console.log(random);
 
 
 /*********************************************
  *              let and const
  ******************************************** */
-// random = Math.floor(Math.random() * x);
-// console.log(random);
+random = Math.floor(Math.random() * x);
+console.log(random);
 
 // TO DO: Refactor everything above to use es6 variable types.
 //     What happens when you change the 'random' variable to a const?
@@ -29,20 +29,20 @@
 // TO DO TOGETHER: change the var keyword to const. What do you see in
 // the console? Now change it to let and try again.
 
-// var sayHello = "Hello World!";
-//
-// sayHello = "Hello Deimos!";
-//
-// console.log(sayHello);
+let sayHello = "Hello World!";
+
+sayHello = "Hello Deimos!";
+
+console.log(sayHello);
 
 
     // TO DO: change var i into let. What are your results?
 
-// var numbers = [1,2,3,4,5];
-//
-// for(let i = 0; i < numbers.length; i++){
-//     console.log(i);
-// }
+var numbers = [1,2,3,4,5];
+
+for(let i = 0; i < numbers.length; i++){
+    console.log(i);
+}
 // console.log(i);
 
 /*********************************************
@@ -50,20 +50,58 @@
  ******************************************** */
     // TO DO TOGETHER: using a for...of loop, console log each element in
     // the array.
-// const myArray = ["it", "is", "really", "cold", "outside"];
+const myArray = ["it", "is", "really", "cold", "outside"];
+
+myArray.forEach(function (element) {
+    console.log(element);
+});
+
+for (let element of myArray){
+    console.log(element);
+}
 //
 // //TO DO: using a for ... of loop, iterate over the array of instructors. If
 // // the instructor is Sophie or David, console log the name.
 //
-// const instructors = [
-//     'Daniel',
-//     'Vivian',
-//     'Sophie',
-//     'David',
-//     'Justin',
-//     'Fer',
-// ];
+const instructors = [
+    'Daniel',
+    'Vivian',
+    'Sophie',
+    'David',
+    'Justin',
+    'Fer',
+];
 
+const [a,b,c,d] = instructors;
+const europaInstructors = [a,b];
+const deimosInstructors = [c,d];
+console.log(a);
+console.log(b);
+console.log(europaInstructors);
+console.log(deimosInstructors);
+
+for (let instructor of instructors){
+    if (instructor === "Sophie" || instructor === "David"){
+        console.log(instructor);
+    }
+}
+
+var nums = [1,2,3,4,5,6,7,8];
+
+function isEven(arr) {
+    let bucket = [];
+
+    for (let num of nums) {
+
+        if (num % 2 === 0) {
+            bucket.push(num);
+
+        }
+    }
+    return bucket;
+}
+
+console.log(isEven(nums));
 
 
 /*********************************************
@@ -76,18 +114,33 @@
 // function add(a,b){
 //     return a + b;
 // }
+
+const add = (a,b) => a + b;
+
+// function square(x){
+//     return x * x;
+// }
+
+const square = x => x * x;
+
 //
 // // TO DO TOGETHER: refactor the following so that variable 'greeting' has a
 // // default value of 'Good Morning' and 'name' has the value of 'Deimos.
 // //
 // //      Check your work.
 //
-// const myGreeting = (greeting,name) => {
-//     return `${greeting},${name}`;
-// };
-//
-// console.log(myGreeting());
-// console.log(myGreeting("Hello", "Sophie"));
+const myGreeting = (greeting = "Good Morning",name = "Deimos") => {
+    return `${greeting},${name}`;
+};
+
+console.log(myGreeting());
+console.log(myGreeting("Hello", "Sophie"));
+
+const male = "Pocky";
+const female = "Polly";
+
+console.log("My pets are " + male + " and " + female);
+console.log(`My pets are ${male} and ${female}. I have ${add(1,1)} cats`);
 
 /*********************************************
  *              Objects
@@ -95,33 +148,45 @@
     
 // TO DO: Refactor the Object below to use shorthand property assignment
 
-// const mydog = "Spike";
-// const cat = "Tom";
-// const mouse = "Jerry";
-//
-// const pals = {
-//     dog: mydog,
-//     cat: cat,
-//     mouse: mouse
-// };
+let mydog = "Spike";
+let cat = "Tom";
+let mouse = "Jerry";
+
+const pals = {
+    dog: mydog,
+    cat,
+    mouse
+};
+
+console.log(pals.cat);
+console.log(pals.mouse);
+console.log(pals.dog);
 //
 // // TO DO TOGETHER: Use Object Destructuring to refactor
 //
-// const make = 'nissan';
-// const model= 'rogue';
-// const color = 'white';
-//
-// const myCar = {
-//     make,
-//     model,
-//     color
-// };
-//
-// const carInfo = (car) => {
-//     var make = car.make;
-//     var model = car.model;
-// };
-//
-// carInfo(myCar);
+const make = 'nissan';
+const model= 'rogue';
+const color = 'white';
 
+const myCar = {
+    make,
+    model,
+    color
+};
+
+const youCar = {
+    make: "Hyundai",
+    model: "Sonata",
+    color: "purple"
+};
+
+const carInfo = ({make,model}) => {
+    // let make = car.make;
+    // let model = car.model;
+    console.log(make,model);
+};
+
+carInfo(myCar);
+carInfo(youCar);
 //BONUS: Array Destructuring.
+
