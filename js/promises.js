ef0fc02a82685138e3f99ffef0509a40e048ce90
Kellsey Neagley <kellseyneagley@gmail.com>
2019-11-13 16:30:35 -0600

    revised promises

--- js/promises.js
+++ js/promises.js
@@ -1,12 +1,12 @@
 "use strict";
 
-function wait(num) {
+const wait = num => {
     return new Promise((resolve) => {
         setTimeout(() => {
                 resolve();
             },num);
     });
-}
+};
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
